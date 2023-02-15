7ad6be4f2ea964ca362499cd69500e86f671bb21
Luceniente <124631319+Luceniente@users.noreply.github.com>
2023-02-15 14:43:23 +0000

    Add files via upload

---
+++ myData.js
@@ -0,0 +1,3 @@
+const myArray1 = [50, 60, 70];
+let mydata=myArray1.at(0);
+console.log(mydata);
---
+++ myStr.js
@@ -0,0 +1,3 @@
+let chaine1="This is the start";
+let chaine2="This is the end";
+console.log(chaine1 + " "  +chaine2)
